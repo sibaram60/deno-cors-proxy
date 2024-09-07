d659945e632dfd10ed174a054d4c3757e9276808
sibaram60 <sibaram.nayak@sudayconsultats.com>
2024-09-07 21:15:49 +0530

    root url expose

--- routers/forward-routes.ts
+++ routers/forward-routes.ts
@@ -1,7 +1,30 @@
 // deno-lint-ignore-file no-explicit-any
-import { Router, Request, Response } from "https://deno.land/x/oak@v17.0.0/mod.ts";
+import {
+  Router,
+  Request,
+  Response,
+} from "https://deno.land/x/oak@v17.0.0/mod.ts";
 
 const router: Router = new Router();
+
+router.get(
+  "/",
+  ({ request, response }: { request: Request; response: Response }) => {
+    try {
+      // const url = request.url.searchParams.get("url") as string;
+      // if (!url) {
+      //   throw new Error("No url provided");
+      // }
+      // const result = await fetch(url, {
+      //   method: "GET",
+      //   headers: { "X-Requested-With": "XMLHttpRequest" },
+      // });
+      response.body = "Hello!, This is Cors proxy service.";
+    } catch (err) {
+      throw err;
+    }
+  }
+);
 
 router.get(
   "/forward",
