7a871a1913a64d0db0625e971b9b471f39f1e86f
sibaram60 <sibaram.nayak@sudayconsultats.com>
2024-09-08 10:47:58 +0530

    Cors enabled

--- routers/forward-routes.ts
+++ routers/forward-routes.ts
@@ -1,140 +1,116 @@
 // deno-lint-ignore-file no-explicit-any
-import {
-  Router,
-  Request,
-  Response,
-} from "https://deno.land/x/oak@v17.0.0/mod.ts";
+import { Router, Request, Response } from 'https://deno.land/x/oak@v17.0.0/mod.ts';
 
 const router: Router = new Router();
 
-router.get(
-  "/",
-  ({ request, response }: { request: Request; response: Response }) => {
-    try {
-      // const url = request.url.searchParams.get("url") as string;
-      // if (!url) {
-      //   throw new Error("No url provided");
-      // }
-      // const result = await fetch(url, {
-      //   method: "GET",
-      //   headers: { "X-Requested-With": "XMLHttpRequest" },
-      // });
-      response.body = "Hello!, This is Cors proxy service.";
-    } catch (err) {
-      throw err;
-    }
-  }
-);
+router.get('/', ({ response }: { response: Response }) => {
+	try {
+		response.headers.append('Access-Control-Allow-Origin', '*');
+		response.body = 'Hello!, This is Cors proxy service. this adds Cors headers to requests';
+	} catch (err) {
+		throw err;
+	}
+});
 
-router.get(
-  "/forward",
-  async ({ request, response }: { request: Request; response: Response }) => {
-    try {
-      const url = request.url.searchParams.get("url") as string;
-      if (!url) {
-        throw new Error("No url provided");
-      }
-      const result = await fetch(url, {
-        method: "GET",
-        headers: { "X-Requested-With": "XMLHttpRequest" },
-      });
-      response.body = await result.json();
-    } catch (err) {
-      throw err;
-    }
-  }
-);
+router.get('/forward', async ({ request, response }: { request: Request; response: Response }) => {
+	try {
+		const url = request.url.searchParams.get('url') as string;
+		if (!url) {
+			throw new Error('No url provided');
+		}
+		const result = await fetch(url, {
+			method: 'GET',
+			headers: { 'X-Requested-With': 'XMLHttpRequest' },
+		});
+		response.headers.append('Access-Control-Allow-Origin', '*');
+		response.body = await result.json();
+	} catch (err) {
+		throw err;
+	}
+});
 
-router.post(
-  "/forward",
-  async ({ request, response }: { request: Request; response: Response }) => {
-    try {
-      const url = request.url.searchParams.get("url") as string;
-      if (!url) {
-        throw new Error("No url provided");
-      }
-      const body = JSON.stringify(request.body);
-      const result = await fetch(url, {
-        method: "POST",
-        body,
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-        },
-      });
-      return (response.body = await result.json());
-    } catch (err: any) {
-      throw err;
-    }
-  }
-);
+router.post('/forward', async ({ request, response }: { request: Request; response: Response }) => {
+	try {
+		const url = request.url.searchParams.get('url') as string;
+		if (!url) {
+			throw new Error('No url provided');
+		}
+		const body = JSON.stringify(request.body);
+		const result = await fetch(url, {
+			method: 'POST',
+			body,
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+			},
+		});
+		response.headers.append('Access-Control-Allow-Origin', '*');
+		return (response.body = await result.json());
+	} catch (err: any) {
+		throw err;
+	}
+});
 
-router.patch(
-  "/forward",
-  async ({ request, response }: { request: Request; response: Response }) => {
-    try {
-      const url = request.url.searchParams.get("url") as string;
-      if (!url) {
-        throw new Error("No url provided");
-      }
-      const body = JSON.stringify(request.body);
-      const result = await fetch(url, {
-        method: "Patch",
-        body,
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-        },
-      });
-      response.body = await result.json();
-    } catch (err: any) {
-      throw err;
-    }
-  }
-);
+router.patch('/forward', async ({ request, response }: { request: Request; response: Response }) => {
+	try {
+		const url = request.url.searchParams.get('url') as string;
+		if (!url) {
+			throw new Error('No url provided');
+		}
+		const body = JSON.stringify(request.body);
+		const result = await fetch(url, {
+			method: 'Patch',
+			body,
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+			},
+		});
+		response.headers.append('Access-Control-Allow-Origin', '*');
+		response.body = await result.json();
+	} catch (err: any) {
+		throw err;
+	}
+});
 
-router.delete(
-  "/forward",
-  async ({ request, response }: { request: Request; response: Response }) => {
-    try {
-      const url = request.url.searchParams.get("url") as string;
-      if (!url) {
-        throw new Error("No url provided");
-      }
-      const body = JSON.stringify(request.body);
-      const result = await fetch(url, {
-        method: "DELETE",
-        body,
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-        },
-      });
-      response.body = await result.json();
-    } catch (err: any) {
-      throw err;
-    }
-  }
-);
+router.delete('/forward', async ({ request, response }: { request: Request; response: Response }) => {
+	try {
+		const url = request.url.searchParams.get('url') as string;
+		if (!url) {
+			throw new Error('No url provided');
+		}
+		const body = JSON.stringify(request.body);
+		const result = await fetch(url, {
+			method: 'DELETE',
+			body,
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+			},
+		});
+		response.headers.append('Access-Control-Allow-Origin', '*');
+		response.body = await result.json();
+	} catch (err: any) {
+		throw err;
+	}
+});
 
-router.put(
-  "/forward",
-  async ({ request, response }: { request: Request; response: Response }) => {
-    try {
-      const url = request.url.searchParams.get("url") as string;
-      if (!url) {
-        throw new Error("No url provided");
-      }
-      const body = JSON.stringify(request.body);
-      const result = await fetch(url, {
-        method: "PUT",
-        body,
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-        },
-      });
-      response.body = await result.json();
-    } catch (err: any) {
-      throw err;
-    }
-  }
-);
+router.put('/forward', async ({ request, response }: { request: Request; response: Response }) => {
+	try {
+		const url = request.url.searchParams.get('url') as string;
+		if (!url) {
+			throw new Error('No url provided');
+		}
+		const body = JSON.stringify(request.body);
+		const result = await fetch(url, {
+			method: 'PUT',
+			body,
+			headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+			},
+		});
+		response.headers.append('Access-Control-Allow-Origin', '*');
+		response.body = await result.json();
+	} catch (err: any) {
+		throw err;
+	}
+});
 
 export { router };
--- server.ts
+++ server.ts
@@ -1,45 +1,18 @@
-import { Application } from "https://deno.land/x/oak@v17.0.0/mod.ts";
-// import { swaggerDoc } from "https://deno.land/x/deno_swagger_doc@releavev2.0.1/mod.ts";
+import { Application } from 'https://deno.land/x/oak@v17.0.0/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts';
+import { router } from './routers/forward-routes.ts';
 
-import { router } from "./routers/forward-routes.ts";
-
-const port = Deno.env.get("PORT") || 5000;
-
-// const swaggerDefinition = {
-//   info: {
-//     // API informations (required)
-//     title: "Cors proxy", // Title (required)
-//     version: "1.0.0", // Version (required)
-//     description: "A sample API", // Description (optional)
-//   },
-//   host: `localhost:${port}`, // Host (optional)
-//   basePath: "/", // Base path (optional)
-//   swagger: "2.0", // Swagger version (optional)
-// };
-
-// // Options for the swagger docs
-// const options = {
-//   // Import swaggerDefinitions
-//   swaggerDefinition,
-//   // Path to the API docs
-//   // Note that this path is relative to the current directory from which the Node.js is ran, not the application itself.
-//   apis: ["./routers/forward-routes.ts"],
-// };
-
-// // Initialize swagger-jsdoc -> returns validated swagger spec in json format
-// const swaggerSpec = swaggerDoc(options);
+const port = Deno.env.get('PORT') || 5000;
 
 const app = new Application();
 
-// app.use(async (context, next) => {
-//   if (context.request.url.pathname === "/swagger.json") {
-//     context.response.headers.set("Content-Type", "application/json");
-//     context.response.status = 200;
-//     context.response.body = swaggerSpec;
-//   } else {
-//     await next();
-//   }
-// });
+app.use(
+	oakCors({
+		origin: '*', // Allow all origins, you can specify specific origins here
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+		allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
+	}),
+);
 
 router.use(router.routes());
 
